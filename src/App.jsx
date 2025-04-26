a302a200e350c0d1ddc1a2169a289b99161c4e24
Mathilde4 <mathibassadou@gmail.com>
2025-04-26 20:20:31 +0000

    modification du design de service

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,39 @@ import './App.css'
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from './components/Sidebar.jsx'
 import ClientList from './pages/clients/ClientList.jsx'
+import ClientCreate from './pages/clients/ClientCreate.jsx';
+import ClientEdit from './pages/clients/ClientEdit.jsx';
+import Login from './pages/Login.jsx';
+import Home from './pages/home/Home.jsx';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
 
-  {/*
-     <div>
-      <Login />
-    </div>*/}
+  
+  const hideSidebarRoutes = ['/login', '/'];
+
+  const shouldHideSidebar = hideSidebarRoutes.includes(location.pathname);
 
   return (
-    <BrowserRouter>
-      <div className='flex'>
-        <Sidebar></Sidebar>
-        <div className='p-7 text-2xl font-semibold flex-1 h-screen'>
-          <Routes>
-            <Route path='/clients' element={<ClientList />} />
-            {/* <Route path='/' element={<ClientList />}/> */}
-          </Routes>
-        </div>
+    <div className='flex'>
+      {!shouldHideSidebar && <Sidebar />}
+      <div className='p-7 text-2xl font-semibold flex-1 h-screen'>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Home />} /> {/* si tu as une page Home */}
+          <Route path='/clients' element={<ClientList />} />
+          <Route path='/clients/create' element={<ClientCreate />} />
+          <Route path='/clients/edit/:id' element={<ClientEdit />} />
+        </Routes>
       </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   )
 }
