170707816be78fb296fde31495813e4d1b3efcaa
okook659 <araulbora@gmail.com>
2025-04-26 20:42:47 +0000

    Revert "finition du header du homepage"
    This reverts commit 43b8ed437d7a9c4d81e7ec034536c21ab513de91.
    
    Resolution des conflits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import './Navbar.css'
 
 function Navbar() {
   return (
-    <div className='nav-bar'>
-      <span>SCW</span>
-      <div className="navbar-links">
-        <ul>
-          <li>Accueil</li>
-          <li>Services</li>
-          <li>Contacts</li>
-        </ul>
-      </div>  
-        <a href="/login" className='btn'>Connexion</a>
-    </div>
+    <div>Navbar</div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,8 @@
-import Navbar from "../../components/Navbar"
-import './Home.css'
-import car3 from '../../assets/car3.jpg';
-function Home() {
-    return (
-        <>
-            <div className="header">
-                <Navbar />
-                <div className="header-content">
-                    <span>Bienvenue à Super Car Wash</span>
-                    <img id="header-content-image" src={car3} alt="Voiture propre" />
-                </div>
-            </div>
-        </>
-
+function Home(){
+    return(
+        <div>
+            
+        </div>
     )
 }
 export default Home
